787273fe1f5d3087bdce6e5d908cf3607a395146
Dane <max_power297@hotmail.com>
2018-03-03 14:07:41 -0500

    back to old website

--- horoscope.py
+++ horoscope.py
@@ -15,7 +15,7 @@ zodiac = {'aries': 'https://imgur.com/TAgbGIE.png','taurus': 'https://imgur.com/
 'sagittarius': 'https://imgur.com/Mw5FyG6.png', 'capricorn': 'https://imgur.com/ZCgXIMU.png', 'aquarius': 'https://imgur.com/YiOam4U.png', 'pisces': 'https://imgur.com/YFDcGHO.png'}
 
 
-weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
 @client.event
 async def on_message(message):
@@ -23,31 +23,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('-h'):
+    if message.content.startswith('+['):
         args = message.content.split(" ")
         sign = args[1]
         sign = sign.lower()
         if sign in zodiac:
-            req = Request('http://astrostyle.com/daily-horoscopes/' + sign + '-daily-horoscope/',
+            req = Request('https://www.astrology.com/horoscope/daily/' + sign + '.html',
                           headers={'User-Agent': 'Mozilla/5.0'})
             url = urlopen(req)
             content = url.read()
+            embed=discord.Embed(title="", color=0x808080)
             soup = BeautifulSoup(content, "html.parser")
             o = datetime.datetime.now().strftime('%Y/%m/%d')
-            todays_date = date.today()
             td = datetime.date.today().strftime("%A").lower()
             if td in weekdays:
-                i = soup.find("div", {"id": td }).findAll('p')[0].next
+                i = soup.find("div", {"daily-horoscope"}).findAll('p')[0].next
             else:
                 i = "shits broke"
 
 
-            embed=discord.Embed(title="", color=0x808080)
             embed.set_thumbnail(url=zodiac[sign])
             embed.add_field(name="sign", value=sign, inline=False)
             embed.add_field(name="horoscope", value=i, inline=False)
             embed.set_footer(text="Date: " + o + " | " + "astrostyle.com")
-            """schedule.every(24).hours.do(await client.send_message(message.channel, "```"))"""
 
             await client.send_message(message.channel, embed=embed)
         elif sign == "":
